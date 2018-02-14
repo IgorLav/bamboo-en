21b115eb24fafb5d98a4f648b48d6d706b300d08
IgorLav <i.lavskij@gmail.com>
2018-02-14 10:36:20 +0200

    add js handler for mob nav

--- js/main.js
+++ js/main.js
@@ -3,6 +3,26 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
+    function mobNavToggle () {
+        var mobNav = $('.header .nav-wrap');
+        var burger = $('.burger-btn');
+        var body = $('.body');
+
+        burger.click(function (e) {
+            $(this).addClass('active');
+            mobNav.addClass('active');
+            body.addClass('hidden');
+        });
+
+        $('.mob-nav-close').click(function (e) {
+            burger.removeClass('active');
+            mobNav.removeClass('active');
+            body.removeClass('hidden');
+        });
+    }
+
+    mobNavToggle();
+
     var stickyNav = $('.header');
     var introSectionHeight = $('.intro-section');
 
