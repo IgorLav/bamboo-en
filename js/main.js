ae0a609a93c8821e71e4acadd05fa5dbd8f91540
IgorLav <i.lavskij@gmail.com>
2018-02-14 23:24:07 +0200

    ilavs

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     function mobNavToggle () {
         var mobNav = $('.header .nav-wrap');
         var burger = $('.burger-btn');
-        var body = $('.body');
+        var body = $('body');
 
         burger.click(function (e) {
             $(this).addClass('active');
@@ -33,4 +33,24 @@ $(document).ready(function () {
             top: setOffset()
         }
     });
+
+    // anime effects
+    (function($) {
+        $.fn.animated = function(inEffect, outEffect) {
+            $(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+                if (dir === "down") {
+                    $(this).addClass(inEffect).css("opacity", "1");
+                } else {
+                    $(this).addClass(outEffect).css("opacity", "1");
+                }
+            }, {
+                offset: "80%"
+            });
+        };
+    })($);
+
+
+
+    $(".feature-section .description-wrap, .partnership-section .img-wrap img").animated("fadeInLeft");
+    $(".hero .text-box, .partnership-section .description-wrap").animated("fadeInRight");
 });
