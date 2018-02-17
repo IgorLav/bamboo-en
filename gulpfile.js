bd0bc805de4d86124a1d934044f66e0e1f072a60
IgorLav <i.lavskij@gmail.com>
2018-02-17 22:38:13 +0200

    migrate to scss

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp'),
+    scss = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber');
+
+
+
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
+
+gulp.task('styles', function () {
+    return gulp.src('./src/sass/main.scss')
+        .pipe(plumber({ errorHandler: handleError }))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./src/css'));
+});
+
+
+gulp.task('watch', ['styles'], function () {
+    gulp.watch('./sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles'], function () {
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function () {
+    jQuery('a[href="#"]').click(function (e) {
+        e.preventDefault();
+    });
+
+    function mobNavToggle () {
+        var mobNav = jQuery('.header .nav-wrap');
+        var burger = jQuery('.burger-btn');
+        var body = jQuery('body');
+
+        burger.click(function (e) {
+            jQuery(this).addClass('active');
+            mobNav.addClass('active');
+            body.addClass('hidden');
+        });
+
+        jQuery('.mob-nav-close').click(function (e) {
+            burger.removeClass('active');
+            mobNav.removeClass('active');
+            body.removeClass('hidden');
+        });
+    }
+
+    mobNavToggle();
+
+    var stickyNav = jQuery('.header');
+    var introSectionHeight = jQuery('.intro-section');
+
+    stickyNav.affix({
+        offset: {
+            top: 10
+        }
+    });
+
+    // animate effects
+    function sectionAnim () {
+        (function(jQuery) {
+            jQuery.fn.animated = function(inEffect, outEffect) {
+                jQuery(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+                    if (dir === "down") {
+                        jQuery(this).addClass(inEffect).css("opacity", "1");
+                    } else {
+                        jQuery(this).addClass(outEffect).css("opacity", "1");
+                    }
+                }, {
+                    offset: "80%"
+                });
+            };
+        })(jQuery);
+
+        jQuery(".fromLeft").animated("fadeInLeft");
+        jQuery(".fromRight").animated("fadeInRight");
+        jQuery(".fromDown").animated("fadeInUp");
+    }
+
+    if(jQuery.waypoints) {
+        sectionAnim();
+    }
+
+    //tabs
+    jQuery('.page-tabs-nav a').click(function (e) {
+        e.preventDefault();
+        var tabId = jQuery(this).attr('href');
+
+        jQuery('.page-tabs-nav a').removeClass('active');
+        jQuery('.tab-content').removeClass('active').hide();
+        jQuery(this).addClass('active');
+            jQuery(tabId).fadeIn(600).addClass('active');
+    });
+});
